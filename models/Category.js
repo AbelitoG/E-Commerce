8d56916bc7d47d13e71497d5919e43a311fb34fe
Abel Gonzales <abelgonzales425@gmail.com>
2022-10-26 15:36:39 -0600

    seeded db

--- models/Category.js
+++ models/Category.js
@@ -8,12 +8,12 @@ Category.init(
   {
     // define columns
     id: {
-      type: DataTypes.INTERGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
-    catagory_name:{
+    category_name:{
       type: DataTypes.STRING,
       allowNull: false
     }
--- models/index.js
+++ models/index.js
@@ -5,10 +5,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Catagory, {foreignKey:'catagory_id'})
+Product.belongsTo(Category, {foreignKey:'category_id'})
 
 // Categories have many Products
-Category.hasMany(Product, {foreignKey: 'catagory_id'})
+Category.hasMany(Product, {foreignKey: 'category_id'})
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {through: ProductTag, foreignKey: 'product_id'})
