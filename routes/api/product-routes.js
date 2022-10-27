08984d7f9d11fe991731f641f4740669e7c6e6cd
Abel Gonzales <abelgonzales425@gmail.com>
2022-10-27 16:13:42 -0600

    finished routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const data = await Product.findAll({include: [Category, {model: Tag, through: ProductTag}]})
+  res.json(data)
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const data = await Product.findByPk(req.pamams.id, {include:[Category, {model: Tag, through: ProductTag}]})
+  res.json(data)
 });
 
 // create new product
@@ -89,8 +93,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  const data = await Product.destroy({where: {id: req.params.id}})
+  res.json(data)
 });
 
 module.exports = router;
